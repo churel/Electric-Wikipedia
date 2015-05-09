cfcc03be28c71b2dfd1d826daf65a0693afbeba2
Clément Hurel <clement.hurel@gmail.com>
2015-05-08 21:52:52 -0400

    fix issue with quick menu

--- themes/warmupedia/javascript/article.js
+++ themes/warmupedia/javascript/article.js
@@ -1,31 +1,42 @@
-
 +function() {
 	$( document ).ready(function() {
-	 $('.not-sidebar-content  img').each(function() {
-	 	$(this).removeAttr('width');
-	 	$(this).removeAttr('height');
-	 });
-
-	
-    // Target your .container, .wrapper, .post, etc.
-    $(".not-sidebar-content").fitVids();
+		$('.not-sidebar-content  img').each(function() {
+		 	$(this).removeAttr('width');
+		 	$(this).removeAttr('height');
+		});
+	    $(".not-sidebar-content").fitVids();
 
 		var dropdown = $('.quick-menu .categorie.dropdown');
 		$(dropdown).find('ul.quick-articles-list').show();
-		var activeDropdown = {active : dropdown};
+
+		var activeDropdown = {active : $('.quick-menu .categorie.dropdown > a')};
+
 		$('.quick-menu .categorie > a').click(function() {
-			if(!$(this).is(activeDropdown.active) ) {
-				$(activeDropdown.active).parent().find('ul.quick-articles-list').slideUp('0.5');
-				$(activeDropdown.active).parent().removeClass('dropdown');
-				$(this).parent().find('ul.quick-articles-list').slideDown('0.5');
-				$(this).parent().addClass('dropdown');
-				activeDropdown.active = $(this);
-			}
-			else {
-				$(activeDropdown.active).find('ul.quick-articles-list').slideUp('0.5');
-				$(activeDropdown.active).removeClass('dropdown');
-				activeDropdown.active = false;
-			}
+				if(!$(this).is(activeDropdown.active) ) {
+					var self = this;
+					if(activeDropdown.active == false) {
+						$(this).parent().find('ul.quick-articles-list').slideDown('0.5','swing', function() {
+						});
+					}
+					else {
+						$(activeDropdown.active).parent().find('ul.quick-articles-list').slideUp('0.5','swing', function() {
+							$(self).parent().find('ul.quick-articles-list').slideDown('0.5');
+						});
+					}
+					
+					
+					$(activeDropdown.active).parent().removeClass('dropdown');
+					
+					$(this).parent().addClass('dropdown');
+					activeDropdown.active = $(this);
+					}
+				else {
+					$(activeDropdown.active).parent().find('ul.quick-articles-list').slideUp('0.5','swing',function() {
+					});
+					$(activeDropdown.active).parent().removeClass('dropdown');
+					activeDropdown.active = false;
+				}
+			//}
 		});
 	});
 }(jQuery)
